9ddc9efc7d5a66f14f00944712880c1c923c61f6
Prime <wolabash@gmail.com>
2025-03-22 10:35:18 +0000

    Feat: Updated DB Documentation

--- src/main/resources/db/migration/V2__Create_products_table.sql
+++ src/main/resources/db/migration/V2__Create_products_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS  products (
                           id SERIAL PRIMARY KEY,
-                          farmer_id BIGINT NOT NULL,
+                          farmer_id INT NOT NULL,
                           name VARCHAR(255) NOT NULL,
                           category ENUM('vegetable', 'fruit', 'grain', 'dairy', 'meat') NOT NULL,
                           price DECIMAL(10,2) NOT NULL,
