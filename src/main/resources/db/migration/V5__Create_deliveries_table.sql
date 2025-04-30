6be9a96b6804496b1bfa5ef85020efec4ca22b45
Lallana <Lallanainfinity@gmail.com>
2025-04-30 20:27:55 +0100

    Feat: Refactoring the flyway package for agroconnect

--- src/main/resources/db/migration/V5__Create_deliveries_table.sql
+++ src/main/resources/db/migration/V5__Create_deliveries_table.sql
@@ -1,10 +1,13 @@
+-- Step 5: Create the Deliveries Table
 CREATE TABLE IF NOT EXISTS deliveries (
                                           id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                                           order_id BIGINT UNSIGNED NOT NULL,
                                           deliveryman_id BIGINT UNSIGNED NULL,
-                                          third_party_service VARCHAR(100) NULL,
-                                          status ENUM('pending', 'out for delivery', 'delivered') DEFAULT 'pending',
+                                          third_party_service TEXT NULL,
+                                          status ENUM('PENDING', 'OUT_FOR_DELIVERY', 'DELIVERED') DEFAULT 'PENDING',
                                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                                           FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
-                                          FOREIGN KEY (deliveryman_id) REFERENCES users(id) ON DELETE SET NULL
+                                          FOREIGN KEY (deliveryman_id) REFERENCES users(id) ON DELETE SET NULL,
+                                          INDEX (order_id),
+                                          INDEX (deliveryman_id)
 );
